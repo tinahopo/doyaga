da86f6b2ddd3adb8fa8b4e124cd18a8a5dc30f5f
雷古娜 <tinahopo@gmail.com>
2021-08-31 00:15:17 +0800

    Update 2021-08-30T16:15:09.854Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,12 +1,13 @@
+"use strict";
+
 $(function () {
   console.log('Hello Bootstrap5');
 });
-
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 2,
   grid: {
     rows: 2,
-    fill: 'row',
+    fill: 'row'
   },
   spaceBetween: 30,
   allowSlideNext: false,
@@ -17,21 +18,20 @@ var swiper = new Swiper(".mySwiper", {
       slidesPerView: 1.5,
       grid: {
         rows: 1,
-        fill: 'row',
+        fill: 'row'
       },
       spaceBetween: 30,
       allowSlideNext: true,
       allowSlidePrev: true,
-      allowTouchMove: true,
-    },
+      allowTouchMove: true
+    }
   }
 });
-
 var swiper = new Swiper(".Listenswiper", {
   slidesPerView: 1,
   grid: {
     rows: 3,
-    fill: 'row',
+    fill: 'row'
   },
   spaceBetween: 30,
   allowSlideNext: true,
@@ -42,130 +42,110 @@ var swiper = new Swiper(".Listenswiper", {
       slidesPerView: 2,
       grid: {
         rows: 2,
-        fill: 'row',
+        fill: 'row'
       },
       spaceBetween: 30,
       allowSlideNext: true,
       allowSlidePrev: true,
-      allowTouchMove: true,
+      allowTouchMove: true
     },
     1280: {
       slidesPerView: 3,
       grid: {
         rows: 2,
-        fill: 'row',
+        fill: 'row'
       },
       spaceBetween: 30,
       allowSlideNext: false,
       allowSlidePrev: false,
-      allowTouchMove: false,
-    },
+      allowTouchMove: false
+    }
   }
 });
-
 var swiper = new Swiper(".swiperLevel", {
   slidesPerView: 1,
-
   grid: {
     rows: 3,
-    fill: 'column',
+    fill: 'column'
   },
   spaceBetween: 30,
-
-
   breakpoints: {
     992: {
       slidesPerView: 2.1,
       grid: {
         rows: 1,
-        fill: 'row',
+        fill: 'row'
       },
-      spaceBetween: 30,
-
+      spaceBetween: 30
     },
     1400: {
       slidesPerView: 3,
       grid: {
         rows: 1,
-        fill: 'row',
+        fill: 'row'
       },
-      spaceBetween: 30,
+      spaceBetween: 30
     }
   }
 });
-
 var swiper = new Swiper(".swiperTeacher", {
   slidesPerView: 1,
-
   grid: {
     rows: 4,
-    fill: 'column',
+    fill: 'column'
   },
   spaceBetween: 30,
-
-
   breakpoints: {
     992: {
       slidesPerView: 2.1,
       grid: {
         rows: 1,
-        fill: 'row',
+        fill: 'row'
       },
-      spaceBetween: 30,
-
+      spaceBetween: 30
     },
     1400: {
       slidesPerView: 3.2,
       grid: {
         rows: 1,
-        fill: 'row',
+        fill: 'row'
       },
-      spaceBetween: 30,
+      spaceBetween: 30
     }
   }
-
 });
-
 var swiper = new Swiper(".recommendSwiper", {
   slidesPerView: 1.2,
   grid: {
     rows: 1,
-    fill: 'row',
+    fill: 'row'
   },
   spaceBetween: 30,
-
   breakpoints: {
     768: {
       slidesPerView: 3,
       grid: {
         rows: 1,
-        fill: 'row',
+        fill: 'row'
       },
-      spaceBetween: 30,
-
+      spaceBetween: 30
     },
     1280: {
       slidesPerView: 4,
       grid: {
         rows: 1,
-        fill: 'row',
+        fill: 'row'
       },
-      spaceBetween: 30,
-
-    },
+      spaceBetween: 30
+    }
   }
 });
-
-const elem = document.querySelector('input[name="datepicker"]');
-const datepicker = new Datepicker(elem, {
+var elem = document.querySelector('input[name="datepicker"]');
+var datepicker = new Datepicker(elem, {
   autohide: true,
-  nextArrow: `<span class="material-icons">
-chevron_right
-</span>`,
-  prevArrow: `<span class="material-icons">
-chevron_left
-</span>`,
+  nextArrow: "<span class=\"material-icons\">\nchevron_right\n</span>",
+  prevArrow: "<span class=\"material-icons\">\nchevron_left\n</span>",
   buttonClass: "btn primary",
-  todayHighlight: true,
+  todayHighlight: true
 });
-
+//# sourceMappingURL=all.js.map
